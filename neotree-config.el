d70cdfe87f7327b54b6ce210cfb418550a600914
Cenan ÖZEN <me@cenanozen.com>
2018-07-04 22:11:29 +0300

    Disable line numbers on neotree

--- neotree-config.el
+++ neotree-config.el
@@ -7,6 +7,12 @@
 (evil-define-key 'normal neotree-mode-map (kbd "RET") 'neotree-enter)
 (define-key evil-normal-state-map (kbd ",n") 'neotree-find)
 
-(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+(setq neo-theme (if (display-graphic-p) 'arrow))
 (setq neo-confirm-create-file 'off-p)
 (setq neo-create-file-auto-open t)
+
+(defun display-line-numbers-disable-hook ()
+  "Disable display-line-numbers locally"
+  (display-line-numbers-mode -1))
+
+(add-hook 'neo-after-create-hook (lambda (&optional dummy) (display-line-numbers-disable-hook)))
